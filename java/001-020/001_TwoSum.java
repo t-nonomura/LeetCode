404df46d93cda2c4aa787a5496e278a93a3e6fff
t-nonomura <nonomura0812@gmail.com>
2019-05-14 17:47:12 +0900

    001_TwoSum

---
+++ java/001-020/001_TwoSum.java
@@ -0,0 +1,21 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap()<>;
+        for (int i = 0; i < nums.length; i++) {
+            int oneOfPair = target - int[i];
+            if (map.containsKey(oneOfPair)) return new int[] { map.get(oneOfPair), i}
+            map.put(nums[i], i);
+        }
+        // 存在しない場合
+        throw new IllegalArgumentException("no solution");
+    }
+}
